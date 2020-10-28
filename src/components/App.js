4c2d2790748a83a305df7a4398f8de8d5a83b7c5
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-10-28 05:47:07 -0600

    check if a user is logged in with checkLoginStatus

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import axios from "axios";
 import Home from './Home';
 import Dashboard from './Dashboard';
 
@@ -13,6 +14,28 @@ class App extends Component {
     }
 
     this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  checkLoginStatus() {
+    axios.get('http://localhost:3001/logged_in', { withCredentials: true })
+    .then((response) => {
+      if(response.data.logged_in && this.state.loggedInStatus === 'NOT_LOGGED_IN')
+        this.setState({ 
+          loggedInStatus: 'LOGGED_IN',
+          user: response.data.user
+        })
+
+      else if(!response.data.logged_in && this.state.loggedInStatus === 'LOGGED_IN')
+        this.setState({ 
+          loggedInStatus: 'NOT_LOGGED_IN',
+          user: {}
+        })
+
+    }).catch((error) => console.log('login? error: ', error));
+  }
+
+  componentDidMount() {
+    this.checkLoginStatus()
   }
 
   handleLogin(data) {
