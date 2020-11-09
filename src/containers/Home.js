10e554d6d3e74d64eef8947b413d5c5d48b9145c
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-11-09 11:11:41 -0600

    added error basic styling and method recatoring on Home

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -8,40 +8,43 @@ class Home extends Component {
   constructor(props) {
     super(props);
 
-    this.renderErrors = this.renderErrors.bind(this);
+    this.state = {
+      errors: false,
+      errorArray: []
+    }
   }
 
 
   componentDidMount() {
     this.props.handleSignStatus().then(
       () => {
-        
         if(this.props.logged_in) 
           this.props.history.push('/dashboard');
       })
   }
 
-  renderErrors() {
-    let errors = this.props.errors;
-    let renderText = [];
-
-    if (errors.length !== undefined) { 
-      const jsonErrors = JSON.parse(errors);
-      for(let key in jsonErrors) {
-        if (jsonErrors.hasOwnProperty(key))
-          renderText.push(jsonErrors[key][0]);
-      }
-    }
-
-    return renderText.map((text, index) => <p key={index} className='home-error'>{text}</p>);
-  }
-
   render() {
+
+    let { errors } = this.props;
+    let containerClass = '';
+    let jsonErrors = {};
+    let errorItems = [];
+      
+    if (errors.length !== undefined) {
+      containerClass = 'error-container'
+      jsonErrors = JSON.parse(errors);
+      for(let key in jsonErrors)
+        if (jsonErrors.hasOwnProperty(key))
+          errorItems.push(jsonErrors[key]);
+    }
+    else containerClass = 'hidden';
 
     return (
       <div className='home'>
-      <div className='error-container'>
-        {this.renderErrors()}
+      <div className={containerClass}>
+        {
+          errorItems.map((error, index) => <p key={index} className='error-item'>{error}</p>)
+        }
       </div>
         <div className='title'><h1>Tasktracker</h1></div>
         <Registration homeProps={this.props} />
